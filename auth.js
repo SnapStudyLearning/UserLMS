cbb1e1b1283e12055ab34311ce35d1617bb5eba5
SnapStudyLearning <contact.snapstudy1@gmail.com>
2025-06-07 15:28:08 +1000

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,6 @@
+firebase.auth().onAuthStateChanged(user => {
+  if (!user) window.location.href = 'login.html';
+});
+function logout() {
+  firebase.auth().signOut().then(() => window.location.href = "login.html");
+}
